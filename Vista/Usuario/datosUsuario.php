defb8f7b217bd2732a07b96ea1bb51f03e3ec0d0
betorojas <jhmorenor@unbosque.edu.co>
2018-05-28 11:11:08 -0500

    funcionando

--- Vista/Usuario/datosUsuario.php
+++ Vista/Usuario/datosUsuario.php
@@ -155,31 +155,54 @@ $usuarios = UsuarioControlador::usuLC($id);
                 </div>
               </div>
             </div>
+
+
+            <?php 
+ if(!empty($_SESSION['cart'])){
+
+   ?>
+
+
+
+            <div class="row">
+              <?php 
+          $key ="4Vj8eK4rloUd272L48hsrarnUA";
+          $merchantId=508029;
+           $accountId=512321;
+           $referenceCode=date("d-m-Y h:i:s");
+           $amount=$cart->get_total_payment();
+           $currency="COP";
+           $correo =  $_SESSION["usuario"]["correo"];        
+           $ding = md5($key."~".$merchantId."~".$referenceCode."~".$amount."~".$currency);
+           $url = 'http://'.$_SERVER["SERVER_NAME"].'/cakes/vista/usuario/compraRespuesta.php';
+           ?>
+              <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
+                <input name="merchantId" type="hidden" value="<?php echo $merchantId?>">
+                <input name="accountId" type="hidden" value="<?php  echo $accountId?>">
+                <input name="description" type="hidden" value="VENTASCAKES">
+                <input name="referenceCode" type="hidden" value="<?php echo $referenceCode?>">
+                <input name="amount" type="hidden" value="<?php echo $amount?>">
+                <input name="tax" type="hidden" value="" <input name="taxReturnBase" type="hidden" value="2">
+                <input name="currency" type="hidden" value="<?php echo $currency?>">
+                <input name="signature" type="hidden" value="<?php  echo $ding ?>">
+                <input name="test" type="hidden" value="1">
+                <input name="buyerEmail" type="hidden" value="<?php echo $correo?>">
+                <input name="responseUrl" type="hidden" value="<?php echo $url ?>">
+                <!-- <input name="confirmationUrl"    type="hidden"  value="http://www.test.com/confirmation" >  -->
+                <button class="btn waves-effect waves-light pink pulse" name="Submit" type="submit" value="Realizar Compra"> Realizar Compra</button>
+              </form>
+              <?php
+  }
+  ?>
+
+            </div>
           </div>
-
-          <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/">
-            <input name="merchantId" type="hidden" value="508029">
-            <input name="accountId" type="hidden" value="512321">
-            <input name="description" type="hidden" value="Test PAYU">
-            <input name="referenceCode" type="hidden" value="TestPayU">
-            <input name="amount" type="hidden" value="1000">
-            <input name="tax" type="hidden" value="3193">
-            <input name="taxReturnBase" type="hidden" value="16806">
-            <input name="currency" type="hidden" value="COP">
-            <input name="signature" type="hidden" value="7e970e41a70bca6c23117f1fab2ee28e">
-            <input name="test" type="hidden" value="1">
-            <input name="buyerEmail" type="hidden" value="test@test.com">
-            <input name="responseUrl" type="hidden" value="http://www.test.com/response">
-            <input name="confirmationUrl" type="hidden" value="http://www.test.com/confirmation">
-            <input name="Submit" type="submit" value="Enviar">
-
-
-            <!-- Producto -->
+          <!-- Producto -->
 
 
 
 
-            <!-- ciere row -->
+          <!-- ciere row -->
         </div>
 
         <!-- cierra de container principal -->
