e49081637ba74227b34c67793ad58895219e081f
betorojas <jhmorenor@unbosque.edu.co>
2018-05-28 11:55:07 -0500

    cambios

--- Vista/Usuario/datosUsuario.php
+++ Vista/Usuario/datosUsuario.php
@@ -102,8 +102,16 @@ $usuarios = UsuarioControlador::usuLC($id);
               </div>
 
             </div>
+            <center>
 
+<ul class="hide-on-med-and-down ">
+  <li>
+    <a class="waves-effect waves-light btn modal-trigger pink darken-1" href="#modal2">editar </a>
+  </li>
+</ul>
+</center>
           </div>
+
 
           <!-- fin de tab datosU -->
           <div id="comprasU">
@@ -157,43 +165,43 @@ $usuarios = UsuarioControlador::usuLC($id);
             </div>
 
 
-            <?php 
- if(!empty($_SESSION['cart'])){
+            <?php
+if (!empty($_SESSION['cart'])) {
 
-   ?>
+    ?>
 
 
 
             <div class="row">
-              <?php 
-          $key ="4Vj8eK4rloUd272L48hsrarnUA";
-          $merchantId=508029;
-           $accountId=512321;
-           $referenceCode=date("d-m-Y h:i:s");
-           $amount=$cart->get_total_payment();
-           $currency="COP";
-           $correo =  $_SESSION["usuario"]["correo"];        
-           $ding = md5($key."~".$merchantId."~".$referenceCode."~".$amount."~".$currency);
-           $url = 'http://'.$_SERVER["SERVER_NAME"].'/cakes/vista/usuario/compraRespuesta.php';
-           ?>
+              <?php
+$key = "4Vj8eK4rloUd272L48hsrarnUA";
+    $merchantId = 508029;
+    $accountId = 512321;
+    $referenceCode = date("d-m-Y h:i:s");
+    $amount = $cart->get_total_payment();
+    $currency = "COP";
+    $correo = $_SESSION["usuario"]["correo"];
+    $ding = md5($key . "~" . $merchantId . "~" . $referenceCode . "~" . $amount . "~" . $currency);
+    $url = 'http://' . $_SERVER["SERVER_NAME"] . '/cakes/vista/usuario/compraRespuesta.php';
+    ?>
               <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
-                <input name="merchantId" type="hidden" value="<?php echo $merchantId?>">
-                <input name="accountId" type="hidden" value="<?php  echo $accountId?>">
+                <input name="merchantId" type="hidden" value="<?php echo $merchantId ?>">
+                <input name="accountId" type="hidden" value="<?php echo $accountId ?>">
                 <input name="description" type="hidden" value="VENTASCAKES">
-                <input name="referenceCode" type="hidden" value="<?php echo $referenceCode?>">
-                <input name="amount" type="hidden" value="<?php echo $amount?>">
+                <input name="referenceCode" type="hidden" value="<?php echo $referenceCode ?>">
+                <input name="amount" type="hidden" value="<?php echo $amount ?>">
                 <input name="tax" type="hidden" value="" <input name="taxReturnBase" type="hidden" value="2">
-                <input name="currency" type="hidden" value="<?php echo $currency?>">
-                <input name="signature" type="hidden" value="<?php  echo $ding ?>">
+                <input name="currency" type="hidden" value="<?php echo $currency ?>">
+                <input name="signature" type="hidden" value="<?php echo $ding ?>">
                 <input name="test" type="hidden" value="1">
-                <input name="buyerEmail" type="hidden" value="<?php echo $correo?>">
+                <input name="buyerEmail" type="hidden" value="<?php echo $correo ?>">
                 <input name="responseUrl" type="hidden" value="<?php echo $url ?>">
                 <!-- <input name="confirmationUrl"    type="hidden"  value="http://www.test.com/confirmation" >  -->
                 <button class="btn waves-effect waves-light pink pulse" name="Submit" type="submit" value="Realizar Compra"> Realizar Compra</button>
               </form>
               <?php
-  }
-  ?>
+}
+?>
 
             </div>
           </div>
@@ -219,8 +227,7 @@ $usuarios = UsuarioControlador::usuLC($id);
             <label>
               <h4>Editar Cuenta:</h4>
             </label>
-            <input disabled type="text" name="id_usuario" id="id_usuario" style="display:none" required value="<?php echo $_SESSION["
-              usuario "]["id_usuario "]; ?>  ">
+            <input disabled type="text" name="id_usuario" id="id_usuario" style="display:none" required value="<?php echo $_SESSION["usuario"]["id_usuario"]; ?>  ">
             <div class="input-field col s6 m6 l6">
               <label>Ciudad</label>
               <input class="validate" type="text" name="ciudad" id="ciudad" pattern="[a-z]{1,15}" required value="<?php echo $usua['ciudad']; ?>">
